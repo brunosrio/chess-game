66fdb987617b880970442873a25e8b479b889d77
Bruno Osório <bruno.osorio.carvalho.almeida@ccc.ufcg.edu.br>
2023-12-08 11:03:25 -0300

    First class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import entities.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position p = new Position(3, 5);
+        System.out.println(p);
+    }
+}
